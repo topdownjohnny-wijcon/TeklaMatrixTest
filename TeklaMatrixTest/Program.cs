fef40fd25a6813b1b36eb3ad16e61e94ecf3e2ac
John via Wijcon <38974971+topdownjohnny-wijcon@users.noreply.github.com>
2022-09-12 14:57:40 +0200

    Update Program.cs
    variable names

--- TeklaMatrixTest/Program.cs
+++ TeklaMatrixTest/Program.cs
@@ -8,33 +8,34 @@ namespace TeklaMatrixTest
     {
         public static void Main()
         {
-            var teklaAssembly=Assembly.GetAssembly(typeof(Matrix)).GetName().Version;
+            var teklaAssembly = Assembly.GetAssembly(typeof(Matrix)).GetName().Version;
             Console.WriteLine($"Test: {teklaAssembly}");
             Console.WriteLine();
 
-            var lokaalMiddelpunt2 = new Vector(-77.071613073349, 14.500000953674316, 0.0);
-            Console.WriteLine(lokaalMiddelpunt2);
+            var point = new Vector(-77.071613073349, 14.500000953674316, 0.0);
+            Console.WriteLine(point);
 
 
-            var lokaalNaarWereld2 = new Matrix();
-            lokaalNaarWereld2[0, 0] = 0;
-            lokaalNaarWereld2[0, 1] = 1;
-            lokaalNaarWereld2[0, 2] = 0;
-            lokaalNaarWereld2[1, 0] = -1;
-            lokaalNaarWereld2[1, 1] = 0;
-            lokaalNaarWereld2[1, 2] = 0;
-            lokaalNaarWereld2[2, 0] = 0;
-            lokaalNaarWereld2[2, 1] = 0;
-            lokaalNaarWereld2[2, 2] = 1;
-            lokaalNaarWereld2[3, 0] = 10125;
-            lokaalNaarWereld2[3, 1] = 13427.0716166353;
-            lokaalNaarWereld2[3, 2] = 1250;
-            Console.WriteLine(lokaalNaarWereld2);
+            var matrix = new Matrix();
+            matrix[0, 0] = 0;
+            matrix[0, 1] = 1;
+            matrix[0, 2] = 0;
+            matrix[1, 0] = -1;
+            matrix[1, 1] = 0;
+            matrix[1, 2] = 0;
+            matrix[2, 0] = 0;
+            matrix[2, 1] = 0;
+            matrix[2, 2] = 1;
+            matrix[3, 0] = 10125;
+            matrix[3, 1] = 13427.0716166353;
+            matrix[3, 2] = 1250;
+            Console.WriteLine(matrix);
 
-            var trans2 = MatrixFactory.FromCoordinateSystem(new CoordinateSystem(lokaalMiddelpunt2, new Vector(1, 0, 0), new Vector(0, 1, 0)));
-            Console.WriteLine(trans2);
-            lokaalNaarWereld2 = lokaalNaarWereld2 * trans2;
-            Console.WriteLine(lokaalNaarWereld2);
+            var transform = MatrixFactory.FromCoordinateSystem(new CoordinateSystem(point, new Vector(1, 0, 0), new Vector(0, 1, 0)));
+            Console.WriteLine(transform);
+            
+            matrix = matrix * transform;
+            Console.WriteLine(matrix);
         }
     }
 }
