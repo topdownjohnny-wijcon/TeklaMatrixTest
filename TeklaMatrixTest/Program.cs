d0b37d8b7ea6efc13ef1838d7a1c98ff47341d73
John via Wijcon <38974971+topdownjohnny-wijcon@users.noreply.github.com>
2022-09-12 15:01:08 +0200

    Update Program.cs
    Only show last matrix

--- TeklaMatrixTest/Program.cs
+++ TeklaMatrixTest/Program.cs
@@ -8,13 +8,13 @@ namespace TeklaMatrixTest
     {
         public static void Main()
         {
+            // Write out assembly version, make sure to REBUILD when changing versions!
             var teklaAssembly = Assembly.GetAssembly(typeof(Matrix)).GetName().Version;
             Console.WriteLine($"Test: {teklaAssembly}");
             Console.WriteLine();
 
-            var point = new Vector(-77.071613073349, 14.500000953674316, 0.0);
-            Console.WriteLine(point);
-
+            var vector = new Vector(-77.071613073349, 14.500000953674316, 0.0);
+            //Console.WriteLine(vector);
 
             var matrix = new Matrix();
             matrix[0, 0] = 0;
@@ -29,10 +29,10 @@ namespace TeklaMatrixTest
             matrix[3, 0] = 10125;
             matrix[3, 1] = 13427.0716166353;
             matrix[3, 2] = 1250;
-            Console.WriteLine(matrix);
+            //Console.WriteLine(matrix);
 
-            var transform = MatrixFactory.FromCoordinateSystem(new CoordinateSystem(point, new Vector(1, 0, 0), new Vector(0, 1, 0)));
-            Console.WriteLine(transform);
+            var transform = MatrixFactory.FromCoordinateSystem(new CoordinateSystem(vector, new Vector(1, 0, 0), new Vector(0, 1, 0)));
+            //Console.WriteLine(transform);
             
             matrix = matrix * transform;
             Console.WriteLine(matrix);
